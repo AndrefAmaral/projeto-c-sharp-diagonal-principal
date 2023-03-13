bd6fd697f7aac1e1ce2c228e5cbe80d83f7dccf3
Andre Felipe <andrefelipe.pi2@gmail.com>
2023-03-13 17:33:16 -0300

    Exercicio de matrizes

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Diagonal_Principal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n,n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i,j] = int.Parse(values[j]);
+                }
+            }
+
+            Console.WriteLine("Diagonal principal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+            int cont = 0;
+             for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i,j] < 0)
+                    {
+                        cont++;
+                    } 
+                }
+            }
+            
+            Console.WriteLine("Números negativos: " + cont);
+        }
+    }
+}
